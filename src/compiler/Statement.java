0915f0b85450475f586882eef631d0d02ab74a55
Rainbow_He <yuqinghe75@gmail.com>
2023-04-23 15:20:20 -0400

    Attempt to write genCode on Statement.java

--- src/compiler/Statement.java
+++ src/compiler/Statement.java
@@ -13,6 +13,44 @@ import x64codegen.*;
 import dataflow.*;
 
 public abstract class Statement {
-	protected abstract void print(String s,FileWriter f)throws IOException;
-	protected abstract void genCode(Function f);
+    protected abstract void print(String s, FileWriter f) throws IOException;
+
+    protected abstract void genCode(Function f);
+
+	// This is probably very very wrong...just an educational guess T-T
+	// Add this variable to determine statement type
+    protected enum StmtType {
+        ExprStatement,
+        CompStatement,
+        IterStatement,
+        RetStatement,
+        SelsStatement
+    }
+
+    protected abstract StmtType getStmtType();
+
+    protected StmtType stmtType = StmtType.ExprStatement;
+
+    protected void genCode(Function f, int regNum) {
+        switch (getStmtType()) {
+            case ExprStatement:
+                ((ExprStatement) this).genCode(f, regNum);
+                break;
+            case CompStatement:
+                ((CompStatement) this).genCode(f, regNum);
+                break;
+            case IterStatement:
+                ((IterStatement) this).genCode(f, regNum);
+                break;
+            case RetStatement:
+                ((RetStatement) this).genCode(f, regNum);
+                break;
+            case SelsStatement:
+                ((SelsStatement) this).genCode(f, regNum);
+                break;
+            default:
+                throw new RuntimeException("Invalid Statement Type");
+        }
+    }
 }
+
